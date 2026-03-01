603228138578982cdc3941d78382baee1b88fdd7
Tom Wiesing <tkw01536@gmail.com>
2026-03-01 04:20:29 +0100

    PasswordWrapper: cleanup

--- src/login/pages/Login/PasswordWrapper.tsx
+++ src/login/pages/Login/PasswordWrapper.tsx
@@ -1,5 +1,4 @@
 import type { JSX } from "keycloakify/tools/JSX";
-import { clsx } from "keycloakify/tools/clsx";
 import { useIsPasswordRevealed } from "keycloakify/tools/useIsPasswordRevealed";
 import type { I18n } from "../../i18n";
 
@@ -18,22 +17,18 @@ export type PasswordWrapperProps = {
 export default function PasswordWrapper(props: PasswordWrapperProps) {
     const { i18n, passwordInputId, invalid, children } = props;
     const { msgStr } = i18n;
-    const { isPasswordRevealed, toggleIsPasswordRevealed } = useIsPasswordRevealed({
-        passwordInputId
-    });
+    const { isPasswordRevealed, toggleIsPasswordRevealed } = useIsPasswordRevealed({ passwordInputId });
 
     return (
-        <div className={clsx(styles.inputGroup, invalid && styles.inputGroupInvalid)}>
+        <div className={styles.inputGroup} data-invalid={invalid}>
             {children}
             <button
                 type="button"
-                className={styles.visibilityButton}
                 aria-label={msgStr(isPasswordRevealed ? "hidePassword" : "showPassword")}
                 aria-controls={passwordInputId}
                 onClick={toggleIsPasswordRevealed}
             >
                 <i
-                    className={styles.visibilityIcon}
                     aria-hidden
                     data-password-visibility-label={msgStr(
                         isPasswordRevealed ? "hidePassword" : "showPassword"
